b2e24f6c3a2aad13411c5018757955263c219019
Otavio Henrique de Lima e Silva <otaviohlsilva@gmail.com>
2023-04-21 17:41:12 -0300

    commit 4

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -45,15 +45,41 @@ class _LoginState extends State<Login> {
                           alignment: Alignment.centerLeft,
                           child: Text(
                             "Email",
+                            style: TextStyle(
+                              color: Color.fromRGBO(11, 59, 96, 1),
+                              fontWeight: FontWeight.bold,
+                              fontSize: 16,
+                            ),
                           ),
                         ),
-                        TextFormField(
-                          controller: _emailController,
-                          decoration: const InputDecoration(
-                            hintText: "Insira seu endereço de email",
-                            hintStyle: TextStyle(fontSize: 16),
+                        const SizedBox(height: 8),
+                        Container(
+                          padding: const EdgeInsets.symmetric(horizontal: 16),
+                          decoration: BoxDecoration(
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.grey.withOpacity(0.2),
+                                // color: Color.fromRGBO(248, 248, 248, 1),
+                                spreadRadius: 0,
+                                blurRadius: 6,
+                                offset: const Offset(2, 2),
+                              ),
+                            ],
+                            border: Border.all(
+                              width: 1,
+                              color: Colors.grey.withOpacity(0.2),
+                            ),
+                            borderRadius: BorderRadius.circular(8),
                           ),
-                          style: const TextStyle(fontSize: 16),
+                          child: TextFormField(
+                            controller: _emailController,
+                            decoration: const InputDecoration(
+                              hintText: "Insira seu endereço de email",
+                              hintStyle: TextStyle(fontSize: 16),
+                              border: InputBorder.none,
+                            ),
+                            style: const TextStyle(fontSize: 16),
+                          ),
                         ),
                       ],
                     ),
