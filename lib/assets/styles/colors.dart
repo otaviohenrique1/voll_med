f71d950ef79fa54e9406d1005e6b84a1f30b4308
Otavio Henrique de Lima e Silva <otaviohlsilva@gmail.com>
2023-04-24 22:37:12 -0300

    commit 7

--- lib/assets/styles/colors.dart
+++ lib/assets/styles/colors.dart
@@ -1,4 +1,15 @@
 import 'package:flutter/material.dart';
 
-const Color azulEscuro = Color.fromRGBO(11, 59, 96, 1);
-const Color cinzaEscuro = Color.fromRGBO(144, 152, 159, 1);
+const Color azul1 = Color.fromRGBO(133, 196, 255, 1); /* #85C4FF */
+const Color azul2 = Color.fromRGBO(51, 156, 255, 1); /* #339CFF */
+const Color azul3 = Color.fromRGBO(11, 59, 96, 1); /* #0B3B60 */
+const Color azul4 = Color.fromRGBO(11, 59, 96, 1); /* #00213D */
+const Color azul5 = Color.fromRGBO(194, 225, 255, 1); /* #C2E1FF */
+const Color cinza1 = Color.fromRGBO(231, 235, 239, 1); /* #E7EBEF */
+const Color cinza2 = Color.fromRGBO(144, 152, 159, 1); /* #90989F */
+const Color cinza3 = Color.fromRGBO(107, 110, 113, 1); /* #6B6E71 */
+const Color branco = Colors.white; /* #FFFFFF */
+const Color preto1 = Colors.black; /* #000000 */
+const Color preto2 = Color.fromRGBO(22, 23, 23, 1); /* #161717 */
+const Color vermelhoEscuro = Color.fromRGBO(137, 49, 49, 1); /* #893131 */
+const Color amarelo = Color.fromRGBO(255, 184, 0, 1); /* #FFB800 */
---
+++ lib/assets/styles/styles.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:voll_med/assets/styles/fonts.dart';
+
+var styleH1 = {
+  "fontFamily": fontFamily,
+  "fontWeight": FontWeight.w700,
+  "fontSize": 32,
+  "lineHeight": 40,
+};
+
+var styleH2Bold = {
+  "fontFamily": fontFamily,
+  "fontWeight": FontWeight.w700,
+  "fontSize": 24,
+  "lineHeight": "auto",
+};
+
+var styleH2Regular = {
+  "fontFamily": fontFamily,
+  "fontWeight": FontWeight.w400,
+  "fontSize": 20,
+  "lineHeight": 24.3,
+};
+
+var styleH3Bold = {
+  "fontFamily": fontFamily,
+  "fontWeight": FontWeight.w700,
+  "fontSize": 18,
+  "lineHeight": 21.9,
+};
+
+var styleH3Regular = {
+  "fontFamily": fontFamily,
+  "fontWeight": FontWeight.w400,
+  "fontSize": 18,
+  "lineHeight": 21.9,
+};
+
+var styleParagraphBold = {
+  "fontFamily": fontFamily,
+  "fontWeight": FontWeight.w700,
+  "fontSize": 16,
+  "lineHeight": 19.4,
+};
+
+var styleParagraphRegular = {
+  "fontFamily": fontFamily,
+  "fontWeight": FontWeight.w400,
+  "fontSize": 16,
+  "lineHeight": 19.4,
+};
+
+var styleCaption = {
+  "fontFamily": fontFamily,
+  "fontWeight": FontWeight.w400,
+  "fontSize": 14,
+  "lineHeight": 16,
+};
