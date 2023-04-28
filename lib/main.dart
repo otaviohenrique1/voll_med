f663e2f0f1752b44108e37ebbdd7f48c17be8059
Otavio Henrique de Lima e Silva <otaviohlsilva@gmail.com>
2023-04-27 23:05:16 -0300

    commit 24

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:voll_med/pages/homepage.dart';
 // import 'package:voll_med/pages/cadastro.dart';
-import 'package:voll_med/pages/login.dart';
+// import 'package:voll_med/pages/login.dart';
 
 void main() {
   runApp(const VollMed());
@@ -20,7 +21,8 @@ class VollMed extends StatelessWidget {
             const AppBarTheme(backgroundColor: Color.fromRGBO(0, 33, 61, 1)),
         useMaterial3: true,
       ),
-      home: const Login(),
+      home: const Homepage(),
+      // home: const Login(),
       // home: const Cadastro(),
     );
   }
